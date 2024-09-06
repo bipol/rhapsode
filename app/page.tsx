f00029768b4e3c34c1f89fa4f563519cd6e249b8
Bipol Alam <bipolalam@gmail.com>
2024-09-06 14:35:48 -0400

    remove unneeded

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-import { DailyVoiceClient } from 'realtime-ai-daily';
-import { VoiceClientAudio, VoiceClientProvider } from 'realtime-ai-react';
 import App from '../components/App';
-import { CharacterProvider, CharacterContext } from '../components/context';
+import { CharacterProvider } from '../components/context';
 
 export default function Home() {
   return (
